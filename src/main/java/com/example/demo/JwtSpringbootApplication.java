fa015c2052c41b820683e0c83d518ceb174da78f
denghong <743855298@qq.com>
2022-08-03 18:33:05 +0800

    引入了redis的使用

--- src/main/java/com/example/demo/JwtSpringbootApplication.java
+++ src/main/java/com/example/demo/JwtSpringbootApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
+@EnableCaching
 public class JwtSpringbootApplication {
 
     public static void main(String[] args) {
+        SecurityContextHolder.getContext();
         SpringApplication.run(JwtSpringbootApplication.class, args);
 
     }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Autowired
+    RedisTemplate redisTemplate;
     @GetMapping("/hello")
     public  String hello(){
+        redisTemplate.opsForValue().set("test1","hello");
+        Object obj= redisTemplate.opsForValue().get("test1");
+        Object obj2= redisTemplate.opsForValue().get("test");
         return "hello";
+    }
+    @GetMapping("/")
+    public String index(){
+        redisTemplate.opsForValue().set("test","hello");
+        return "index";
     }
 }
